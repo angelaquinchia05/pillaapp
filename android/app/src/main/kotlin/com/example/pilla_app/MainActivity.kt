f2337abfcc3b756a2d8b28365eacb6c32acc3fde
angela <angelaquinchia05@gmail.com>
2022-04-07 20:44:17 -0500

    Configuracion inicial del proyecto pilla app Ya esta el get y las paginas de inicio creadas

---
+++ android/app/src/main/kotlin/com/example/pilla_app/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.example.pilla_app
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ lib/main.dart
@@ -0,0 +1,5 @@
+import 'package:flutter/material.dart';
+import 'package:get/get.dart';
+
+void main() => runApp(MaterialApp(home: Bienvenidos()));
+
